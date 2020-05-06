dfd91848d480959cfbee1cd82763ab413954f3db
jdorhauer <jdorhauer@yahoo.com>
2020-05-06 12:16:44 -0400

    Initial Commit

---
+++ PalindromeChecker/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word or phrase:");
+            string word = Console.ReadLine().ToLower();
+            Console.WriteLine($"That {(IsPalindrome(word) ? "is" : "is not")} a palindrome.");
+            Console.WriteLine("Press any key to close.");
+            Console.ReadKey();
+        }
+
+        private static bool IsPalindrome(string word)
+        {
+            return new string(word.Reverse().ToArray()) == word;
+        }
+    }
+}
